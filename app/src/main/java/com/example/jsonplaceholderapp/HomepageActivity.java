dfe2e62357800077e91428f3741d06cc6513d4cc
DanBed666 <dupochlast88@gmail.com>
2024-02-25 00:59:00 +0100

    Added homepage layout

--- app/src/main/java/com/example/jsonplaceholderapp/HomepageActivity.java
+++ app/src/main/java/com/example/jsonplaceholderapp/HomepageActivity.java
@@ -2,13 +2,44 @@ package com.example.jsonplaceholderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomepageActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class HomepageActivity extends AppCompatActivity
+{
+    FirebaseAuth mAuth;
+    Button posts;
+    Button albums;
+    Button todos;
+    Button my_profile;
+    Button logout;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        mAuth = FirebaseAuth.getInstance();
+        posts = findViewById(R.id.btn_posts);
+        albums = findViewById(R.id.btn_albums);
+        todos = findViewById(R.id.btn_todos);
+        my_profile = findViewById(R.id.btn_myprofile);
+        logout = findViewById(R.id.btn_logout);
+
+        logout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mAuth.signOut();
+                finish();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
     }
 }
