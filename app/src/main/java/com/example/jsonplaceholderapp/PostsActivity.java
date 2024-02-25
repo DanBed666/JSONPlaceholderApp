44830d5daa0b734ff93160fad425674dda520769
DanBed666 <dupochlast88@gmail.com>
2024-02-25 13:20:09 +0100

    Fixed posts recycler view (removed blank spaces, added divider and margins)

--- app/src/main/java/com/example/jsonplaceholderapp/PostsActivity.java
+++ app/src/main/java/com/example/jsonplaceholderapp/PostsActivity.java
@@ -2,18 +2,22 @@ package com.example.jsonplaceholderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PostsActivity extends AppCompatActivity
 {
     RecyclerView recyclerView;
     PostsViewModel postsViewModel;
     PostsAdapter postsAdapter;
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -23,6 +27,9 @@ public class PostsActivity extends AppCompatActivity
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
         recyclerView.setHasFixedSize(true);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL);
+        dividerItemDecoration.setDrawable(Objects.requireNonNull(getDrawable(R.drawable.divider)));
+        recyclerView.addItemDecoration(dividerItemDecoration);
 
         postsViewModel = new PostsViewModel();
 
@@ -38,6 +45,7 @@ public class PostsActivity extends AppCompatActivity
             {
                 postsAdapter = new PostsAdapter(getApplicationContext(), posts);
                 recyclerView.setAdapter(postsAdapter);
+                postsAdapter.notifyDataSetChanged();
             }
         });
     }
